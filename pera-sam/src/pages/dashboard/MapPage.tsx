17c374884724561f8ff8a78cc6f3767fc0933443
zerokali20 <2003zerokali@gmail.com>
2026-09-23 12:56:49 +0530

    remove the openstreet buyed version and add Esri World Street Map

--- pera-sam/src/pages/dashboard/MapPage.tsx
+++ pera-sam/src/pages/dashboard/MapPage.tsx
@@ -573,8 +573,9 @@ export const MapPage = () => {
               scrollWheelZoom={true}
             >
               <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+                maxZoom={19}
               />
 
               {/* User Location Marker */}
